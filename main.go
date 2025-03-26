905941bf5e40b726f779ed82624e6541859152cd
PEMIDI <rashidi.peyman@gmail.com>
2025-03-27 01:46:14 +0330

    refactor: enhance variables

--- main.go
+++ main.go
@@ -2,46 +2,63 @@ package main
 
 import "fmt"
 
+type Food struct {
+	Name  string
+	Price float64
+}
+
+const (
+	pizzaPrice  = 99.99
+	burgerPrice = 110.0
+	tacoPrice   = 100.0
+)
+
+func generateFood() []Food {
+	return []Food{
+		{Name: "pizza", Price: pizzaPrice},
+		{Name: "burger", Price: burgerPrice},
+		{Name: "Taco", Price: tacoPrice},
+	}
+}
+
 func main() {
 	fmt.Println("Welcome to the CLI Restaurant Reservation System!")
 
-	type Food struct {
-		Name  string
-		Price float64
+	foods := generateFood()
+	for i, food := range foods {
+		// Example: fmt.Printf("%d - %s ($%.2f)\n", i+1, food.Name, food.Price)
+		println(i+1, food.Name)
 	}
 
-	// init the foods
-	pizza := Food{Name: "Pizza", Price: 99.99}
-	burger := Food{Name: "burger", Price: 110.0}
-	taco := Food{Name: "Taco", Price: 100.0}
-
-	// show menu
-	fmt.Println("- 1 - ", pizza.Name, " - ", pizza.Price)
-	fmt.Println("- 2 - ", burger.Name, " - ", burger.Price)
-	fmt.Println("- 3 - ", taco.Name, " - ", taco.Price)
-
-	//get user input
-
-	// init user menu
 	var userMenu []Food
 
-	//check user input
-
 	for {
+		var userInput string
 
-		var userInput int
-		println("Choose a number to select a Food:")
+		println("Choose a number to select a Food: enter 4 to confirm order ")
 		fmt.Scanln(&userInput)
 
-		switch userInput {
-		case 1:
-			userMenu = append(userMenu, pizza)
-		case 2:
-			userMenu = append(userMenu, burger)
-		case 3:
-			userMenu = append(userMenu, taco)
-		case 4:
+		if userInput == "exit" {
 			break
+		}
+
+		switch userInput {
+		case "1":
+			userMenu = append(userMenu, foods[0])
+		case "2":
+			userMenu = append(userMenu, foods[1])
+		case "3":
+			userMenu = append(userMenu, foods[2])
+		case "4":
+			totalPrice := 0.0
+			for _, food := range userMenu {
+				totalPrice += food.Price
+			}
+			fmt.Printf("price is $%.2f", totalPrice)
+			return
+
+		case "exit":
+			fmt.Println("goodbye")
 
 		default:
 			print("Invalid input!")
@@ -49,18 +66,4 @@ func main() {
 
 		fmt.Println(userMenu)
 	}
-
 }
-
-// TODO: Load menu data (hardcoded or from a file).
-
-// TODO: Display the menu with Food name, type, and price.
-// TODO: Allow the user to select Food items from the menu.
-// TODO: Calculate the total price based on selected items.
-// TODO: Confirm the order and display a summary.
-
-// TODO: Allow users to make a reservation by entering name, number of guests, and time.
-// TODO: Store order history in a JSON file or SQLite database.
-// TODO: Implement an "Admin Mode" where an admin can add/remove menu items.
-
-// TODO: Implement a clean exit mechanism for the CLI.
